5b42131d6b6eb1001164cd81767dd033f8dfbf97
chingudl7 <chingudl7@naver.com>
2025-09-16 23:10:36 +0900

    java algorithm study day 5

---
+++ Ex005.java
@@ -0,0 +1,16 @@
+package ex.algorithm;
+
+import java.util.Scanner;
+
+public class Ex005 {
+
+	public static void main(String[] args) {
+		//
+		Scanner sc = new Scanner(System.in);
+		String text = sc.nextLine();
+		System.out.println(text.toLowerCase());
+		
+		
+	}
+
+}
