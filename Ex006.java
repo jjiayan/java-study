351de5cb04cfc43a54eaa2e0df4d070c0312b6d3
chingudl7 <chingudl7@naver.com>
2025-09-17 17:03:23 +0900

    java algorithm study day 6

---
+++ Ex006.java
@@ -0,0 +1,22 @@
+package ex.algorithm;
+
+public class Ex006 {
+
+	public static void main(String[] args) {
+		int n1 = 12;
+		int n2 = 18;
+		
+		int result = 0;
+//		for (int i = 1; i < Math.min(n1, n2); i++) {
+//			if (n1 % i == 0 && n2 % i == 0) {
+//				result = i;
+//			}
+//		}
+		
+		
+		
+		System.out.println(result);
+
+	}
+
+}
